42d85831801ff6b58c208f5c3bf3e16c907552f2
RiFlowTH <riflowth@gmail.com>
2021-09-27 03:18:26 +0700

    style: setup eslint to enforce coding style

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const buildPath = path.resolve(__dirname, 'dist');
 
 const nodeExternals = require('webpack-node-externals');
 const TerserPlugin = require('terser-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 const main = {
   entry: './src/__loader__/App.ts',
@@ -11,6 +12,11 @@ const main = {
       test: /\.ts$/, use: ['ts-loader'], exclude: /node_modules/,
     }],
   },
+  plugins: [
+    new ESLintPlugin({
+      extensions: ['ts'],
+    }),
+  ],
   externals: [nodeExternals()],
   target: 'node',
   output: {
