4f8f7917bf1e7256894bf22da509880223994823
XiaoXuxxxx <aonrok555@gmail.com>
2021-10-12 23:31:10 +0700

    fix: convention variable name

--- src/commands/audio/CommandQueue.ts
+++ src/commands/audio/CommandQueue.ts
@@ -36,18 +36,18 @@ export default class CommandPlay extends AudioCommand {
   }
 
   private getCurrentTrackReplyEmbed(currentTrack: Track): MessageEmbed {
-    const { duration_locale, title, url } = currentTrack.getInfo();
+    const { duration_locale: durationLocale, title, url } = currentTrack.getInfo();
     return new MessageEmbed()
       .setColor('#71368A')
       .setTitle('▶️ Current Track')
-      .setDescription(`\`[0]\` \`[${duration_locale}]\` [${title}](${url})\n`);
+      .setDescription(`\`[0]\` \`[${durationLocale}]\` [${title}](${url})\n`);
   }
 
   private getUpcomingTrackReplyEmbed(upcomingTracks: Track[]): MessageEmbed {
     let description: string = '';
     upcomingTracks?.forEach((element, index) => {
-      const { duration_locale, title, url } = element.getInfo();
-      description += `\`[${index + 1}]\` \`[${duration_locale}]\` [${title}](${url})\n`;
+      const { duration_locale: durationLocale, title, url } = element.getInfo();
+      description += `\`[${index + 1}]\` \`[${durationLocale}]\` [${title}](${url})\n`;
     });
     return new MessageEmbed()
       .setColor('#71368A')
