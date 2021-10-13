87c6bbf7d1685425fbb999b38acaeed1747dae03
Krid Heprakhone <17198802+riflowth@users.noreply.github.com>
2021-10-14 01:55:57 +0700

    perf: register commands only needed (#6)

--- src/commands/CommandManager.ts
+++ src/commands/CommandManager.ts
@@ -1,27 +1,36 @@
 import { REST as DiscordApi } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
-import { CommandInteraction } from 'discord.js';
+import { Client, CommandInteraction } from 'discord.js';
 import Command from '@/commands/Command';
 
 export default class CommandManager {
 
-  private readonly clientId: string;
+  private readonly client: Client;
   private readonly discordApi: DiscordApi;
 
   private readonly commandByLabel = new Map<string, Command>();
 
-  public constructor(clientId: string, discordApi: DiscordApi) {
-    this.clientId = clientId;
+  public constructor(client: Client, discordApi: DiscordApi) {
+    this.client = client;
     this.discordApi = discordApi;
   }
 
   public async register(commands: Command[]): Promise<void> {
     try {
-      const commandsInfo = commands.map((command) => command.getInfo());
+      const registeredCommandMap = await this.client.application.commands.fetch();
+      const registeredCommands = registeredCommandMap.map((command) => command.name);
 
-      await this.discordApi.put(
-        Routes.applicationCommands(this.clientId), { body: commandsInfo },
-      );
+      const applicationId = this.client.application.id;
+      const commandsInfo = commands
+        .map((command) => command.getInfo())
+        .filter((command) => !registeredCommands.includes(command.name));
+
+      if (commandsInfo.length !== 0) {
+        await this.discordApi.put(
+          Routes.applicationCommands(applicationId), { body: commandsInfo },
+        );
+        console.log(`Registered new ${commandsInfo.length} commands to Discord successfully`);
+      }
 
       commands.forEach((command) => {
         this.commandByLabel.set(command.getInfo().name, command);
