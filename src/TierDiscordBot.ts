87aaa9499ad691c0f504386b09887ddb0617384f
riflowth <riflowth@gmail.com>
2022-02-25 18:26:30 +0700

    fix: sometime presence not showing

--- src/TierDiscordBot.ts
+++ src/TierDiscordBot.ts
@@ -15,7 +15,9 @@ export default class TierDiscordBot extends DiscordBot implements TierBot {
   public onReady() {
     console.log(`Logged in as ${this.client.user.tag}`);
 
-    this.client.user.setPresence({ activities: [{ name: 'a chill and soul' }] });
+    setInterval(() => {
+      this.client.user.setPresence({ activities: [{ name: 'a chill and soul' }] });
+    }, 5 * 60 * 1000);
 
     this.commandManager.register([
       new CommandVersion(),
