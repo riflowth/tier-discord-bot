784af7fe2240636b8131f31e320944f4e58ea472
riflowth <riflowth@gmail.com>
2022-05-31 15:04:08 +0700

    fix: eslint config issue

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+odule.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'import',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: __dirname,
+      }
+    }
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+  ],
+
+  rules: {
+    // allow console logging for nodejs
+    'no-console': 'off',
+
+    'import/prefer-default-export': 'off',
+
+    'padded-blocks': [
+      'error',
+      { classes: 'always' },
+    ],
+
+    // except empty line after attribute scope variable declarations
+    '@typescript-eslint/lines-between-class-members': [
+      'error',
+      'always',
+      { exceptAfterSingleLine: true },
+    ],
+  },
+};
