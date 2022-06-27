5b37a9045e5152797109b0b494bf774412aaef19
riflowth <riflowth@gmail.com>
2022-06-27 11:34:28 +0700

    feat: add memory / host detail on version cmd

--- src/commands/common/CommandVersion.ts
+++ src/commands/common/CommandVersion.ts
@@ -1,6 +1,7 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, MessageEmbed } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 import Command, { CommandInfo } from '@/commands/Command';
+import os from 'os';
 
 export default class CommandVersion implements Command {
 
@@ -15,7 +16,24 @@ export default class CommandVersion implements Command {
     const { tag } = interaction.client.user;
     const version = process.env.npm_package_version;
 
-    interaction.reply(`\`${tag}\` running on version \`${version}\``);
+    const rssMem = Math.round(((process.memoryUsage().heapUsed / 1024) / 1024) * 100) / 100;
+    const totalMem = Math.round(((os.totalmem() / 1024) / 1024) * 100) / 100;
+
+    const embed = new MessageEmbed()
+      .setAuthor({
+        name: tag,
+        iconURL: interaction.client.user.avatarURL(),
+        url: 'https://github.com/riflowth/tier-discord-bot',
+      })
+      .setColor('#448ad0')
+      .addFields(
+        { name: 'version', value: version, inline: true },
+        { name: 'host', value: os.hostname(), inline: true },
+        { name: 'run on', value: os.arch(), inline: true },
+        { name: 'memory', value: `${rssMem} MB : ${totalMem} MB` },
+      );
+
+    interaction.reply({ embeds: [embed] });
   }
 
 }
